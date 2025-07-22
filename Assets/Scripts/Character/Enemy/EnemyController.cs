c4cd94fe46d597e9ca95fc53a881187fd0308f6d
huy-ngquoc <22520565@gm.uit.edu.vn>
2025-07-22 18:47:07 +0700

    + Add Enemy.

---
+++ Assets/Scripts/Character/Enemy/EnemyController.cs
@@ -0,0 +1,12 @@
+﻿#nullable enable
+
+namespace Game
+{
+    using UnityEngine;
+
+    public sealed class EnemyController : CharacterController
+    {
+        [SerializeField]
+        private float attackRange = 0;
+    }
+}
