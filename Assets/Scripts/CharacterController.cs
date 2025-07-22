81723d79a6d28dbe9e3708ed80adc8c4595dfba0
huy-ngquoc <22520565@gm.uit.edu.vn>
2025-07-22 18:35:46 +0700

    + Add `CharacterController`.

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,186 @@
+﻿#nullable enable
+
+namespace Game;
+
+using UnityEngine;
+
+public abstract class CharacterController : MonoBehaviour
+{
+    [SerializeReference]
+    [ResolveComponent]
+    private new Rigidbody2D rigidbody2D = null!;
+
+    [SerializeReference]
+    [ResolveComponent]
+    private CapsuleCollider2D capsuleCollider2D = null!;
+
+    [SerializeReference]
+    [ResolveComponentInChildren]
+    private Animator animator = null!;
+
+    private string currentAnimationName = "Idle";
+
+    private bool isFacingRight = true;
+
+    [Header("Character Collision info")]
+
+    [SerializeReference]
+    [ResolveComponentInChildren("Ground Check")]
+    private Transform groundCheck = null!;
+
+    [SerializeField]
+    [LayerMaskIsNothingOrEverythingWarning]
+    private LayerMask groundLayerMask = new();
+
+    [SerializeField]
+    [Range(0.01F, 2)]
+    private float groundCheckDistance = 0.1F;
+
+    [Header("Character Stats")]
+
+    [field: SerializeField]
+    [field: Range(5, 40)]
+    private float moveSpeed = 8;
+
+    private float currentHealth = 100;
+
+    protected CharacterController()
+    {
+    }
+
+    public bool IsGroundDetected => Physics2D.Raycast(
+        this.groundCheck.position,
+        Vector2.down,
+        this.groundCheckDistance,
+        this.groundLayerMask);
+
+    public bool IsFacingRight => this.isFacingRight;
+
+    public int FacingDirection => this.isFacingRight ? 1 : -1;
+
+    public float CurrentHealth => this.currentHealth;
+
+    public bool IsAlive => this.currentHealth > 0;
+
+    public bool IsDead => this.currentHealth <= 0;
+
+    public LayerMask GroundLayerMask => this.groundLayerMask;
+
+    public float MoveSpeed => this.moveSpeed;
+
+    protected Rigidbody2D Rigidbody2D => this.rigidbody2D;
+
+    protected CapsuleCollider2D CapsuleCollider2D => this.capsuleCollider2D;
+
+    public void Init()
+    {
+        this.currentHealth = 100;
+
+        this.OnCharacterControllerInit();
+    }
+
+    public void Despawn()
+    {
+        this.OnCharacterControllerDespawn();
+    }
+
+    public void Hit(float damage)
+    {
+        if (this.currentHealth <= 0)
+        {
+            // Dead already...
+            return;
+        }
+
+        if (this.currentHealth > damage)
+        {
+            this.currentHealth -= damage;
+            return;
+        }
+
+        this.currentHealth = 0;
+        this.Die();
+    }
+
+    protected virtual void OnCharacterControllerInit()
+    {
+        // Leave this method blank
+        // The derived classes can decide if they override this method
+    }
+
+    protected virtual void OnCharacterControllerDespawn()
+    {
+        // Leave this method blank
+        // The derived classes can decide if they override this method
+    }
+
+    protected void ChangeAnimation(string newAnimationName)
+    {
+        if (this.currentAnimationName != newAnimationName)
+        {
+            this.animator.ResetTrigger(this.currentAnimationName);
+            this.currentAnimationName = newAnimationName;
+            this.animator.SetTrigger(this.currentAnimationName);
+        }
+    }
+
+    protected void FlipController(float x)
+    {
+        bool flip = this.IsFacingRight ? (x < 0) : (x > 0);
+        if (flip)
+        {
+            this.Flip();
+        }
+    }
+
+    protected void Awake()
+    {
+        this.OnCharacterControllerAwake();
+    }
+
+    protected virtual void OnCharacterControllerAwake()
+    {
+        // Leave this method blank
+        // The derived classes can decide if they override this method
+    }
+
+    protected void FixedUpdate()
+    {
+        this.OnCharacterControllerFixedUpdate();
+    }
+
+    protected virtual void OnCharacterControllerFixedUpdate()
+    {
+        // Leave this method blank
+        // The derived classes can decide if they override this method
+    }
+
+    protected void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(this.groundCheck.position, new Vector2(this.groundCheck.position.x, this.groundCheck.position.y - this.groundCheckDistance));
+    }
+
+    protected virtual void OnCharacterControllerDrawGizmos()
+    {
+        // Leave this method blank
+        // The derived classes can decide if they override this method
+    }
+
+    protected virtual void OnCharacterControllerDie()
+    {
+        // Leave this method blank
+        // The derived classes can decide if they override this method
+    }
+
+    private void Flip()
+    {
+        this.isFacingRight = !this.isFacingRight;
+        this.transform.Rotate(0, 180, 0);
+    }
+
+    private void Die()
+    {
+        this.OnCharacterControllerDie();
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,27 +4,14 @@ namespace Game
 {
     using UnityEngine;
 
-    public sealed class PlayerController : MonoBehaviour
+    public sealed class PlayerController : CharacterController
     {
         [field: SerializeField]
         [field: ResolveComponent]
         private PlayerInputHandler inputHandler = null!;
 
-        [field: SerializeField]
-        [field: ResolveComponent]
-        private new Rigidbody2D rigidbody2D = null!;
-
-        [field: SerializeField]
-        [field: LayerMaskIsNothingOrEverythingWarning]
-        private LayerMask groundLayerMask = new();
-
-        [field: SerializeField]
-        [field: ResolveComponentInChildren]
-        private Animator animator = null!;
-
         private Vector3 savePoint = Vector3.zero;
         private Vector2 platformVelocity = Vector2.zero;
-        private string currentAnimationName = "Idle";
         private int coinCounter = 0;
         private bool isJumping = false;
         private bool isAttacking = false;
@@ -32,20 +19,14 @@ namespace Game
         private bool isDead = false;
 
         [field: SerializeField]
-        [field: Range(5, 40)]
-        public float MoveSpeed { get; private set; } = 8;
-
-        [field: SerializeField]
         [field: Range(200, 800)]
         public float JumpForce { get; private set; } = 400;
 
-        public bool FacingRight { get; private set; } = true;
+        public void AttackFinishTrigger() => this.isAttacking = false;
 
-        public int FacingDirection => this.FacingRight ? 1 : -1;
+        public void ThrowFinishTrigger() => this.isThrowing = false;
 
-        private bool IsGrounded => Physics2D.Raycast(this.transform.position, Vector2.down, 1.2F, this.groundLayerMask);
-
-        public void OnInit()
+        protected override void OnCharacterControllerInit()
         {
             this.isJumping = false;
             this.isAttacking = false;
@@ -56,85 +37,30 @@ namespace Game
             this.ChangeAnimation("Idle");
         }
 
-        public void AttackFinishTrigger() => this.isAttacking = false;
-
-        public void ThrowFinishTrigger() => this.isThrowing = false;
-
-        private void Attack()
-        {
-            this.isAttacking = true;
-            this.inputHandler.CancelAttackInputAction();
-            this.ChangeAnimation("Attack");
-        }
-
-        private void Throw()
-        {
-            this.isThrowing = true;
-            this.inputHandler.CancelThrowInputAction();
-            this.ChangeAnimation("Throw");
-        }
-
-        private void Jump()
-        {
-            this.inputHandler.CancelJumpInputAction();
-
-            this.isJumping = true;
-            this.rigidbody2D.AddForceY(this.JumpForce);
-            this.ChangeAnimation("Jump");
-        }
-
-        private void FlipController(float x)
-        {
-            bool flip = this.FacingRight ? (x < 0) : (x > 0);
-            if (flip)
-            {
-                this.Flip();
-            }
-        }
-
-        private void Flip()
-        {
-            this.FacingRight = !this.FacingRight;
-            this.transform.Rotate(0, 180, 0);
-        }
-
-        private void ChangeAnimation(string newAnimationName)
-        {
-            if (this.currentAnimationName != newAnimationName)
-            {
-                this.animator.ResetTrigger(this.currentAnimationName);
-                this.currentAnimationName = newAnimationName;
-                this.animator.SetTrigger(this.currentAnimationName);
-            }
-        }
-
-        private void Awake()
+        protected override void OnCharacterControllerAwake()
         {
             this.savePoint = this.transform.position;
-            this.OnInit();
         }
 
-        private void FixedUpdate()
+        protected override void OnCharacterControllerFixedUpdate()
         {
             if (this.isDead || this.isAttacking || this.isThrowing)
             {
-                this.rigidbody2D.linearVelocityX = this.platformVelocity.x;
+                this.Rigidbody2D.linearVelocityX = this.platformVelocity.x;
                 return;
             }
 
-            var isGrounded = this.IsGrounded;
-
             var moveSpeedX = this.inputHandler.MoveInputX * this.MoveSpeed;
             var linearVelocityX = moveSpeedX + this.platformVelocity.x;
-            this.rigidbody2D.linearVelocityX = linearVelocityX;
+            this.Rigidbody2D.linearVelocityX = linearVelocityX;
             this.FlipController(moveSpeedX);
 
-            if (this.rigidbody2D.linearVelocityY <= 0)
+            if (this.Rigidbody2D.linearVelocityY <= 0)
             {
                 this.isJumping = false;
             }
 
-            if (!isGrounded)
+            if (!this.IsGroundDetected)
             {
                 this.ChangeAnimation("Fall");
                 return;
@@ -184,7 +110,7 @@ namespace Game
             {
                 this.isDead = true;
                 this.ChangeAnimation("Die");
-                this.Invoke(nameof(this.OnInit), 1);
+                this.Invoke(nameof(this.Init), 1);
             }
             else if (collision.CompareTag("SpawnPoint"))
             {
@@ -198,7 +124,7 @@ namespace Game
 
         private void OnCollisionStay2D(Collision2D collision)
         {
-            if (Utils.IsLayerInMask(collision.gameObject, this.groundLayerMask))
+            if (Utils.IsLayerInMask(collision.gameObject, this.GroundLayerMask))
             {
                 if (collision.gameObject.TryGetComponent<MovingPlatformController>(out var platform))
                 {
@@ -209,16 +135,33 @@ namespace Game
 
         private void OnCollisionExit2D(Collision2D collision)
         {
-            if (Utils.IsLayerInMask(collision.gameObject, this.groundLayerMask))
+            if (Utils.IsLayerInMask(collision.gameObject, this.GroundLayerMask))
             {
                 this.platformVelocity = Vector2.zero;
             }
         }
 
-        private void OnDrawGizmos()
+        private void Attack()
         {
-            Gizmos.color = Color.red;
-            Gizmos.DrawLine(this.transform.position, this.transform.position + (Vector3.down * 1.2F));
+            this.isAttacking = true;
+            this.inputHandler.CancelAttackInputAction();
+            this.ChangeAnimation("Attack");
+        }
+
+        private void Throw()
+        {
+            this.isThrowing = true;
+            this.inputHandler.CancelThrowInputAction();
+            this.ChangeAnimation("Throw");
+        }
+
+        private void Jump()
+        {
+            this.inputHandler.CancelJumpInputAction();
+
+            this.isJumping = true;
+            this.Rigidbody2D.AddForceY(this.JumpForce);
+            this.ChangeAnimation("Jump");
         }
     }
 }
